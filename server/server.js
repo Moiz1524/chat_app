269a9c78593bb349756c15a3c1b5fadb63f99746
Moiz Ali <amoiz4142@gmail.com>
2019-02-14 18:45:06 +0500

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -15,14 +15,14 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected!');
 
-  socket.emit('newMessage', {
-    from: 'Moiz',
-    text: 'Hey! Whats going on ?',
-    createdAt: new Date()
-  })
-
   socket.on('createMessage', (message) => {
     console.log('Message to look up!', message);
+
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    })
   });
 
   socket.on('disconnect', (socket) => {
